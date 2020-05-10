713e362724e74cfea4634a31ed57e4871b85d29f
kevin-kili <kilikevin435@gmail.com>
2020-05-10 15:02:52 +0300

    added the search movie function

--- app/views.py
+++ app/views.py
@@ -17,14 +17,6 @@ def index():
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
-
 @app.route('/movie/<int:id>')
 def movie(id):
 
@@ -35,3 +27,4 @@ def movie(id):
     title = f'{movie.title}'
 
     return render_template('movie.html',title = title,movie = movie)
+
