fd66accc07cda75f018b47897299b80d5ac289cd
kevin-kili <kilikevin435@gmail.com>
2020-05-10 15:31:42 +0300

    impoerted the reviews

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
-
-from .models import review
 from .forms import ReviewForm
-Review = review.Review
+from .models import reviews
+
+Review = reviews.Review
+
+
 
 # Views
 @app.route('/')
@@ -28,6 +30,7 @@ def index():
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
+
 @app.route('/movie/<int:id>')
 def movie(id):
 
@@ -41,6 +44,7 @@ def movie(id):
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
+
 @app.route('/search/<movie_name>')
 def search(movie_name):
     '''
@@ -52,16 +56,21 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
+
 @app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
+
     form = ReviewForm()
+
     movie = get_movie(id)
 
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
+
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
+
         return redirect(url_for('movie',id = movie.id ))
 
     title = f'{movie.title} review'
